1a8d1d0ce0820c8555008a09ee9e03eeb4e6b6f6
Usama Khaki <khakiu.uk@gmail.com>
2021-10-20 17:09:34 +0400

    Title modified

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Dash-lead Admin panel</title>
+    <title>Dashboard | Usama Khaki APP</title>
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
