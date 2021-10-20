9935fc9c1311463ee80a8967ba0ba9ffb44e672f
Usama Khaki <khakiu.uk@gmail.com>
2021-10-20 11:36:08 +0400

    Revert "dashlead Theme Implemented."
    This reverts commit ee525a11

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PageController::class, 'index'])->name('index');
-Route::get('/sample', [PageController::class, 'sample'])->name('sample');
-Route::get('/table', [PageController::class, 'table'])->name('table');
-Route::get('/datatable', [PageController::class, 'datatable'])->name('datatable');
-Route::get('/404', [PageController::class, 'page404'])->name('404');
+Route::get('/', function () {
+    return view('welcome');
+});
