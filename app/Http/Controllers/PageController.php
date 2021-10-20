3388d4f98a27d0a7798af959a8597483109ab603
Usama Khaki <khakiu.uk@gmail.com>
2021-10-20 17:02:46 +0400

    Sample Pages modified

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -7,24 +7,20 @@ use Illuminate\Http\Request;
 class PageController extends Controller
 {
 
-    public function index(){
-        return view('pages.index');
-    }
-
     public function sample(){
-        return view('pages.sample-page');
+        return view('pages.SamplePages.sample-page');
     }
 
     public function table(){
-        return view('pages.table-basic');
+        return view('pages.SamplePages.table-basic');
     }
 
     public function datatable(){
-        return view('pages.table-data');
+        return view('pages.SamplePages.table-data');
     }
 
     public function page404(){
-        return view('pages.404-page');
+        return view('pages.Errors.404');
     }
 
 }
